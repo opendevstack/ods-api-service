5473269b170c3731207cec4d079880e660345104
Angel Martinez <angelmp.mail@gmail.com>
2026-03-18 14:19:21 +0100

    Refactor ErrorKey enum to use ErrorMessage constants for improved error handling

---
+++ api-project/src/main/java/org/opendevstack/apiservice/project/exception/ErrorMessage.java
@@ -0,0 +1,10 @@
+package org.opendevstack.apiservice.project.exception;
+
+public class ErrorMessage {
+    
+    public static final String NOT_FOUND = "Not Found";
+    public static final String FORBIDDEN = "Forbidden";
+    public static final String BAD_REQUEST = "Bad Request";
+
+    public ErrorMessage() {}
+}
