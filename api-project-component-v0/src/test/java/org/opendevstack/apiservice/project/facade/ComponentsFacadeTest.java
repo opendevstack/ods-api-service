5dff5754f9b53d3439a18b98ebbb8765520fa6d3
zxBCN Valeriu_Tuguran,Constantin (IT EDP) EXTERNAL <constantin.valeriu_tuguran.ext@boehringer-ingelheim.com>
2026-04-29 21:33:27 +0200

    Fix test.

--- api-project-component-v0/src/test/java/org/opendevstack/apiservice/project/facade/ComponentsFacadeTest.java
+++ api-project-component-v0/src/test/java/org/opendevstack/apiservice/project/facade/ComponentsFacadeTest.java
@@ -15,7 +15,6 @@ import org.opendevstack.apiservice.externalservice.marketplace.service.Marketpla
 import org.opendevstack.apiservice.project.exception.ComponentAlreadyExistsException;
 import org.opendevstack.apiservice.project.exception.ComponentCreationException;
 import org.opendevstack.apiservice.project.exception.ComponentNotFoundException;
-import org.opendevstack.apiservice.project.exception.ComponentRetrievalException;
 import org.opendevstack.apiservice.project.mapper.MarketplaceMapper;
 import org.opendevstack.apiservice.project.model.Component;
 import org.opendevstack.apiservice.project.model.ComponentsStatusDTO;
@@ -82,8 +81,8 @@ class ComponentsFacadeTest {
                 .thenReturn(null);
 
         assertThatThrownBy(() -> componentsFacade.getProjectComponent("testProject", "testComponent"))
-                .isInstanceOf(ComponentRetrievalException.class)
-                .hasMessage("Failed to retrieve component 'testComponent' for project 'testProject': Component 'testComponent' not found for project 'testProject'");
+                .isInstanceOf(ComponentNotFoundException.class)
+                .hasMessage("Component 'testComponent' not found for project 'testProject'");
         verify(marketplaceExternalService).getProjectComponent("testProject", "testComponent");
     }
 
